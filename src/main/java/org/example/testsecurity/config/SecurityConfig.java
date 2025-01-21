629b22fa702c12bb79bf19d16c58b106ee0eecfb
JihaeAn <dkswlgo6615@naver.com>
2025-01-21 13:25:59 +0900

    스프링 시큐리티 연습 완성

--- src/main/java/org/example/testsecurity/config/SecurityConfig.java
+++ src/main/java/org/example/testsecurity/config/SecurityConfig.java
@@ -11,33 +11,52 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	// @Bean
+	// public RoleHierarchyImpl roleHierarchy() {
+	//
+	// 	return RoleHierarchyImpl.withDefaultRolePrefix()
+	// 		.role("C").implies("B")
+	// 		.role("B").implies("A")
+	// 		.build();
+	// }
 
-        http
-                // HTTP 요청에 대한 접근 권한을 설정 (람다식으로 진행)
-                .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/", "/login").permitAll() // permitAll: 모두 허용
-                        .requestMatchers("/admin").hasRole("ADMIN")  // hasRole: 해당 롤 가진 사람만 허용
-                        .requestMatchers("/my/**").hasAnyRole("ADMIN", "USER") // hasAnyRole: 롤 여러개 정의
-                        .anyRequest().authenticated()
-                );
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http
-                .formLogin((auth) -> auth.loginPage("/login")
-                .loginProcessingUrl("/loginProc")
-                .permitAll()
-                );
+		http
+			// HTTP 요청에 대한 접근 권한을 설정 (람다식으로 진행)
+			.authorizeHttpRequests((auth) -> auth
+				.requestMatchers("/", "/login", "/join", "/joinProc").permitAll() // permitAll: 모두 허용
+				.requestMatchers("/admin").hasRole("ADMIN")  // hasRole: 해당 롤 가진 사람만 허용
+				.requestMatchers("/my/**").hasAnyRole("ADMIN", "USER") // hasAnyRole: 롤 여러개 정의
+				.anyRequest().authenticated()
+			);
 
-        http
-                .csrf((auth) -> auth.disable());
+		http
+			.formLogin((auth) -> auth.loginPage("/login")
+				.loginProcessingUrl("/loginProc")
+				.permitAll()
+			);
+		// .httpBasic(Customizer.withDefaults());
 
-        return http.build();
-    }
+		http
+			.csrf((auth) -> auth.disable());
+
+		http
+			.sessionManagement((auth) -> auth
+				.maximumSessions(1) // 하나의 아이디에 대한 다중 로그인 허용 개수
+				.maxSessionsPreventsLogin(true)); // 다중 로그인 개수를 초과했을 경우 처리 방법 (true: 새로운 로그인 차단, false: 기존 세션 하나 삭제)
+
+		http
+			.sessionManagement((auth) -> auth
+				.sessionFixation().changeSessionId()); // 로그인 시 동일한 세션에 대한 id 변경
+
+		return http.build();
+	}
 
 }
---
+++ src/main/java/org/example/testsecurity/controller/JoinController.java
@@ -0,0 +1,29 @@
+package org.example.testsecurity.controller;
+
+import org.example.testsecurity.dto.JoinDTO;
+import org.example.testsecurity.service.JoinService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class JoinController {
+
+	@Autowired
+	private JoinService joinService;
+
+	@GetMapping("/join")
+	public String joinP() {
+		return "join";
+	}
+
+	@PostMapping("/joinProc")
+	public String joinProc(JoinDTO joinDTO) {
+
+		System.out.println("joinDTO = " + joinDTO);
+
+		joinService.joinProcess(joinDTO);
+		return "redirect:/login";
+	}
+}
